8015694f0be986b3ac59bf7e83c8fe4d9be7fa3c
Sarah Withee <2601974+geekygirlsarah@users.noreply.github.com>
2024-03-31 12:38:06 -0400

    Fix width on event details page

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ l("URL: " + url);
 
 // This is the Dashboard/page you pick your event from
 if (url.includes("Default.aspx")) {
-    l("Found EventDetails page");
+    l("Found Default page");
     try {
-        jQuery(".container").css("margin-left", "50px");
-        jQuery(".container").css("margin-right", "50px");
+        jQuery(".container").css("margin-left", "5%");
+        jQuery(".container").css("margin-right", "5%");
         jQuery(".container").css("width", "auto");
     }
     catch(e) {
@@ -28,13 +28,16 @@ if (url.includes("EventDetails.aspx")) {
     l("Found EventDetails page");
     try {
         // Make tables wider
-        jQuery(".container").css("margin-left", "50px");
-        jQuery(".container").css("margin-right", "50px");
+        jQuery(".container").css("margin-left", "5%");
+        jQuery(".container").css("margin-right", "5%");
         jQuery(".container").css("width", "auto");
 
         // Show entire table heights
         jQuery("#groupDetail1").css("max-height", "");
         jQuery("#groupDetail2").css("max-height", "");
+
+        // Fix unaccessible color selection
+        //jQuery("div.dayFilled").css("background","")
     }
     catch(e) {
         alert("BetterVMS Debug: " + e);
@@ -43,19 +46,45 @@ if (url.includes("EventDetails.aspx")) {
 
 // This is the page with a role on it
 if (url.includes("RoleScheduling.aspx")) {
-    l("Found EventDetails page");
+    l("Found RoleScheduling page");
+    try {
+        // Make tables wider
+        jQuery(".container").css("margin-left", "auto");
+        jQuery(".container").css("margin-right", "auto");
+
+        // Schedule table
+        // jQuery("#ScheduleView").css("max-width", "");
+        // jQuery("#ScheduleTable").css("width", "auto");
+        // jQuery("#ScheduleView").css("width", "auto");
+        // jQuery("#ScheduleDiv").css("width", "auto");
+        // jQuery("#groupDetail2").css("width", "auto");
+        // jQuery("#ScheduleView").css("width", "auto");
+
+        // List of unassigned applicants
+        // jQuery("#groupDetailUnassigned").css("max-height", "");
+
+        // jQuery("#ActionMessageDiv").css("width","");
+
+        jQuery(".container").css("width", "1100px");
+        jQuery("div").css("max-width", "");
+        //jQuery("div").css("width", "auto");
+    }
+    catch(e) {
+        alert("BetterVMS Debug: " + e);
+    }
+}
+
+// This is the page with reports on it
+if (url.includes("ReportViewer.aspx")) {
+    l("Found ReportViewer page");
     try {
         // Make tables wider
         jQuery(".container").css("margin-left", "50px");
         jQuery(".container").css("margin-right", "50px");
         jQuery(".container").css("width", "auto");
 
-        // Schedule table
-        jQuery("#ScheduleTable").css("width", "auto");
-        jQuery("#ScheduleView").css("max-width", "");
-
-        // List of unassigned applicants
-        jQuery("#groupDetailUnassigned").css("max-height", "");
+        // Table with report data in it
+        jQuery("div#ReportContainer").css("max-width", "100%");
     }
     catch(e) {
         alert("BetterVMS Debug: " + e);
